4eb053f4e17a28fe5ebb952bc338d92c1a703129
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-08-26 15:43:49 +0900

    Fix: AuditorAwareImpl "anonymousUser" 필터링 추가

--- src/main/java/com/sparta/delivery/common/AuditorAwareImpl.java
+++ src/main/java/com/sparta/delivery/common/AuditorAwareImpl.java
@@ -12,7 +12,7 @@ public class AuditorAwareImpl implements AuditorAware<UUID> {
     @Override
     public Optional<UUID> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated() ) {
+        if (authentication == null || !authentication.isAuthenticated() || authentication.getPrincipal().equals("anonymousUser")) {
             return Optional.empty();
         }
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
