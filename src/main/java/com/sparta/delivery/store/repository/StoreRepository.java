984d2d3e56f72210cd9ddbf5c27b3c8f83949fc4
SunMi <wywudi@naver.com>
2024-08-26 16:24:10 +0900

    Feat : 가게 repository추가

---
+++ src/main/java/com/sparta/delivery/store/repository/StoreRepository.java
@@ -0,0 +1,11 @@
+package com.sparta.delivery.store.repository;
+
+import com.sparta.delivery.store.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, UUID> {
+}
