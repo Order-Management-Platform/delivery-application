d511b619d723a25d42eb223acb43256d7670d240
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-08-26 11:38:42 +0900

    Feat: SecurityConfig, User entity, Role 추가

--- src/main/java/com/sparta/delivery/user/entity/User.java
+++ src/main/java/com/sparta/delivery/user/entity/User.java
@@ -1,18 +1,21 @@
 package com.sparta.delivery.user.entity;
 
 import com.sparta.delivery.common.BaseEntity;
+import com.sparta.delivery.user.dto.SignUpRequest;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.Type;
 
 import java.util.UUID;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Table(name = "p_user")
+@Builder
+@SQLRestriction("deleted_at is null")
 public class User extends BaseEntity {
 
     @Id
@@ -37,10 +40,25 @@ public class User extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private UserRole role;
 
-    private boolean isPublic = true;
+    private boolean isPublic;
 
 
+    public static User toEntity(SignUpRequest signUpRequest) {
+        return User.builder()
+                .email(signUpRequest.getEmail())
+                .username(signUpRequest.getUsername())
+                .address(signUpRequest.getAddress())
+                .zipcode(signUpRequest.getZipcode())
+                .nickName(signUpRequest.getNickname())
+                .phone(signUpRequest.getTel())
+                .role(signUpRequest.getRole())
+                .password(signUpRequest.getPassword())
+                .isPublic(true)
+                .build();
+    }
 
 
-
+    public void delete(UUID userId) {
+        this.markDeleted(userId);
+    }
 }
--- src/main/java/com/sparta/delivery/user/entity/UserRole.java
+++ src/main/java/com/sparta/delivery/user/entity/UserRole.java
@@ -1,7 +1,28 @@
 package com.sparta.delivery.user.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum UserRole {
 
-    CUSTOMER, OWNER, MANAGER, MASTER
+    CUSTOMER(Authority.CUSTOMER),
+    OWNER(Authority.OWNER),
+    MANAGER(Authority.MANAGER),
+    MASTER(Authority.MASTER);
+
+
+    private final String authority;
+
+    UserRole(String authority) {
+        this.authority = authority;
+    }
+
+    public static class Authority{
+        public static final String CUSTOMER = "ROLE_CUSTOMER";
+        public static final String OWNER = "ROLE_OWNER";
+        public static final String MANAGER = "ROLE_MANAGER";
+        public static final String MASTER = "ROLE_MASTER";
+
+    }
 
 }
