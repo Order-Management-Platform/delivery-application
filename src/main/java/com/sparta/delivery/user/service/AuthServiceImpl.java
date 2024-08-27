bd3a93ed886cedfe0d9b00e6517f97f517c2d87b
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-08-27 21:21:18 +0900

    ReFactor: role 체크 로직 변경

--- src/main/java/com/sparta/delivery/user/service/AuthServiceImpl.java
+++ src/main/java/com/sparta/delivery/user/service/AuthServiceImpl.java
@@ -1,6 +1,7 @@
 package com.sparta.delivery.user.service;
 
 import com.sparta.delivery.user.entity.User;
+import com.sparta.delivery.user.entity.UserRole;
 import com.sparta.delivery.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,6 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public boolean isRoleMatch(String email, String role) {
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("회원 정보를 찾을 수 없습니다."));
-        return user.getRole().name().equals(role);
+        return userRepository.existsByEmailAndRole(email, UserRole.valueOf(role));
     }
 }
