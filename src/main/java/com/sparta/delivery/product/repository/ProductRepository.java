6ec502fc3d35f2add648e725cc3d21788fff180d
SunMi <wywudi@naver.com>
2024-08-28 15:43:18 +0900

    Feat : "상품 상태변경, 검색목록 조회 코드 추가 "

--- src/main/java/com/sparta/delivery/product/repository/ProductRepository.java
+++ src/main/java/com/sparta/delivery/product/repository/ProductRepository.java
@@ -4,6 +4,9 @@ import com.sparta.delivery.product.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +15,11 @@ import java.util.UUID;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
-    Page<Product> findAllByStoreId(UUID storeId, Pageable pageable);
+    Page<Product> findAllByStoreIdAndNameContaining(UUID storeId,String name, Pageable pageable);
     List<Product> findAllByStoreId(UUID storeId);
+
+    @Modifying
+    @Query("UPDATE Product p SET p.soldOut = :soldOut WHERE p.id = :id")
+    void modifyStatus(@Param("id") UUID productId, @Param("soldOut") boolean soldOut);
+
 }
