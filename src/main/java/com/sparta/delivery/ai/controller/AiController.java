6fc71fc9aab821db4e7bd4cd890ea86166daf0c1
SunMi <wywudi@naver.com>
2024-08-29 01:46:46 +0900

    Fix : responseEntity 통일

--- src/main/java/com/sparta/delivery/ai/controller/AiController.java
+++ src/main/java/com/sparta/delivery/ai/controller/AiController.java
@@ -3,6 +3,8 @@ package com.sparta.delivery.ai.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sparta.delivery.ai.entity.Ai;
 import com.sparta.delivery.ai.service.AiService;
+import com.sparta.delivery.common.ResponseCode;
+import com.sparta.delivery.common.dto.ResponseSingleDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +16,8 @@ public class AiController {
 
     private final AiService aiService;
     @PostMapping
-    public ResponseEntity getAiDescription(@RequestParam String name) throws JsonProcessingException {
-        String response = aiService.getAiDescription(name);
-        return ResponseEntity.ok(response);
+    public ResponseSingleDto getAiDescription(@RequestParam String name) throws JsonProcessingException {
+        String data = aiService.getAiDescription(name);
+        return ResponseSingleDto.of(ResponseCode.SUCC_AI_GET, data);
     }
 }
