026e5b77ca6427d1a50d704f3a68059c392786be
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-08-30 16:55:42 +0900

    Feat: Payment 고객 취소 로직 추가

--- src/main/java/com/sparta/delivery/payment/repositoy/PaymentRepository.java
+++ src/main/java/com/sparta/delivery/payment/repositoy/PaymentRepository.java
@@ -3,7 +3,9 @@ package com.sparta.delivery.payment.repositoy;
 import com.sparta.delivery.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+    Optional<Payment> findByOrderId(UUID orderId);
 }
