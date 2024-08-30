b2358a87d87847436fcaa08c7fa467de666cc565
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-08-30 15:08:13 +0900

    Refactor: @AllArgusConstructor 추가

--- src/main/java/com/sparta/delivery/notice/dto/CreateNoticeRequest.java
+++ src/main/java/com/sparta/delivery/notice/dto/CreateNoticeRequest.java
@@ -1,9 +1,11 @@
 package com.sparta.delivery.notice.dto;
 
 import com.sparta.delivery.notice.entity.NoticeAccess;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class CreateNoticeRequest {
 
     private String title;
