34f3f9cb75e0e6dad6c47e44c898d8e0995cdcca
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-08-31 14:05:07 +0900

    Refactor: Jwt 권한과 실제 권한 체크 로직 수정 - 기존 존재여부 에서 role 값을 가져와서 직접 비교

--- src/main/java/com/sparta/delivery/user/service/AuthServiceImpl.java
+++ src/main/java/com/sparta/delivery/user/service/AuthServiceImpl.java
@@ -1,9 +1,12 @@
 package com.sparta.delivery.user.service;
 
+import com.sparta.delivery.common.ResponseCode;
+import com.sparta.delivery.common.exception.BusinessException;
 import com.sparta.delivery.user.entity.User;
 import com.sparta.delivery.user.entity.UserRole;
 import com.sparta.delivery.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +16,10 @@ public class AuthServiceImpl implements AuthService {
     private final UserRepository userRepository;
 
     @Override
-    public boolean isRoleMatch(String email, String role) {
-        return userRepository.existsByEmailAndRole(email, UserRole.valueOf(role));
+//    @Cacheable(cacheNames = "getRole", key = "#email")
+    public UserRole getRole(String email) {
+        return userRepository.findByEmail(email)
+                .map(User::getRole)
+                .orElseThrow(()-> new BusinessException(ResponseCode.NOT_FOUND_USER));
     }
 }
